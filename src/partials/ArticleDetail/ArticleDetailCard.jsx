6661cfe5d7c567dfed65228b06697804a0a2ecf1
erikprakoso <erikprakoso1993@gmail.com>
2024-05-04 21:43:09 +0700

    refactor: Update ArticleDetailCard component
    This commit refactors the ArticleDetailCard component to improve code readability and maintainability. It introduces a new options object with additional formatting options for the js-beautify library. The options object now includes properties such as brace_style, wrap_line_length, break_chained_methods, and indent_scripts. These changes enhance the formatting of code blocks within the component.
    
    The refactoring also improves the handling of code blocks in the render method. It now checks if the paragraph type is "code" before rendering the code block. This ensures that only code paragraphs are processed as code blocks, preventing any unintended rendering issues.
    
    Additionally, the commit updates the CopyToClipboard component to use the correct text value from the paragraph's children array. This ensures that the correct code is copied when the user clicks the copy button.
    
    Overall, these changes enhance the ArticleDetailCard component by improving code formatting and ensuring correct rendering of code blocks.

--- src/partials/ArticleDetail/ArticleDetailCard.jsx
+++ src/partials/ArticleDetail/ArticleDetailCard.jsx
@@ -13,7 +13,37 @@ export default function ArticleDetailCard() {
   const { id } = useParams();
   const article = useQuery(() => APIArticle.findArticleById(id));
   const { data } = article;
-  const options = { indent_size: 2, space_in_empty_paren: true };
+  const options = {
+    indent_size: 2,
+    space_in_empty_paren: true,
+    brace_style: "collapse",
+    wrap_line_length: 50,
+    break_chained_methods: true,
+    indent_scripts: "keep",
+    max_preserve_newlines: 2,
+    unescape_strings: true,
+    jslint_happy: true,
+    end_with_newline: true,
+    wrap_attributes: "auto",
+    indent_inner_html: true,
+    comma_first: false,
+    e4x: true,
+    indent_char: " ",
+    indent_level: 0,
+    preserve_newlines: true,
+    space_before_conditional: true,
+    wrap_attributes_indent_size: 4,
+    extra_liners: [
+      "html",
+      "head",
+      "body",
+      "/html",
+      "/head",
+      "/body",
+      "script",
+      "style",
+    ],
+  };
 
   // Initialize copiedStates array to track copy status of each code block
   const [copiedStates, setCopiedStates] = useState([]);
@@ -54,45 +84,44 @@ export default function ArticleDetailCard() {
             <div className="mt-4">
               {data?.attributes?.body?.map((paragraph, index) => (
                 <div key={index} className="mb-4">
-                  {paragraph.children.map((child, childIndex) => (
-                    <div key={childIndex} className="mb-1">
-                      {child.code ? (
-                        <div className="relative">
-                          <div className="absolute right-4 top-4 z-10">
-                            <CopyToClipboard
-                              text={child.text}
-                              onCopy={() => handleCopy(index)}
-                            >
-                              <button
-                                className={`px-2 py-1 bg-blue-500 text-white rounded-md hover:bg-blue-600 focus:outline-none focus:bg-blue-600 ${
-                                  copiedStates[index] ? "font-bold" : ""
-                                }`}
-                              >
-                                {copiedStates[index] ? "Copied!" : <FaCopy />}
-                              </button>
-                            </CopyToClipboard>
-                          </div>
-                          <div
-                            className="w-full p-4 rounded-lg"
-                            style={{ backgroundColor: "rgb(40, 42, 54)" }}
+                  {paragraph.type === "code" ? (
+                    <div className="relative">
+                      <div className="absolute right-4 top-4 z-10">
+                        <CopyToClipboard
+                          text={paragraph.children[0].text}
+                          onCopy={() => handleCopy(index)}
+                        >
+                          <button
+                            className={`px-2 py-1 bg-blue-500 text-white rounded-md hover:bg-blue-600 focus:outline-none focus:bg-blue-600 ${
+                              copiedStates[index] ? "font-bold" : ""
+                            }`}
                           >
-                            <div className="top mb-2 flex">
-                              <div className="h-3 w-3 bg-red-500 rounded-full"></div>
-                              <div className="ml-2 h-3 w-3 bg-orange-300 rounded-full"></div>
-                              <div className="ml-2 h-3 w-3 bg-green-500 rounded-full"></div>
-                            </div>
-                            <SyntaxHighlighter
-                              language="javascript"
-                              style={dracula}
-                              showLineNumbers
-                            >
-                              {js_beautify(child.text, options)}
-                            </SyntaxHighlighter>
-                          </div>
+                            {copiedStates[index] ? "Copied!" : <FaCopy />}
+                          </button>
+                        </CopyToClipboard>
+                      </div>
+                      <div
+                        className="w-full p-4 rounded-lg"
+                        style={{ backgroundColor: "rgb(40, 42, 54)" }}
+                      >
+                        <div className="top mb-2 flex">
+                          <div className="h-3 w-3 bg-red-500 rounded-full"></div>
+                          <div className="ml-2 h-3 w-3 bg-orange-300 rounded-full"></div>
+                          <div className="ml-2 h-3 w-3 bg-green-500 rounded-full"></div>
                         </div>
-                      ) : (
+                        <SyntaxHighlighter
+                          language="javascript"
+                          style={dracula}
+                          showLineNumbers
+                        >
+                          {js_beautify(paragraph.children[0].text, options)}
+                        </SyntaxHighlighter>
+                      </div>
+                    </div>
+                  ) : (
+                    paragraph.children.map((child, childIndex) => (
+                      <div key={childIndex} className="mb-1">
                         <span
-                          key={childIndex}
                           className={
                             child.code
                               ? "px-2 py-1 rounded-md"
@@ -103,9 +132,9 @@ export default function ArticleDetailCard() {
                         >
                           {child.text}
                         </span>
-                      )}
-                    </div>
-                  ))}
+                      </div>
+                    ))
+                  )}
                 </div>
               ))}
             </div>
