036a9ce21cf9b0d07cd7e0b3daaeb4f656770e97
erikprakoso <erikprakoso1993@gmail.com>
2024-05-04 13:33:37 +0700

    chore: Add react-copy-to-clipboard package to npm dependencies

--- src/partials/ArticleDetail/ArticleDetailCard.jsx
+++ src/partials/ArticleDetail/ArticleDetailCard.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import { useState } from "react";
 import { useParams } from "react-router-dom";
 import useQuery from "../../hooks/useQuery";
 import APIArticle from "../../services/article.service";
@@ -6,12 +6,28 @@ import { CONST } from "../../utils/Constants";
 import { js_beautify } from "js-beautify";
 import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
 import { dracula } from "react-syntax-highlighter/dist/esm/styles/prism";
+import { CopyToClipboard } from "react-copy-to-clipboard";
+import { FaCopy } from "react-icons/fa";
 
 export default function ArticleDetailCard() {
   const { id } = useParams();
   const article = useQuery(() => APIArticle.findArticleById(id));
   const { data } = article;
   const options = { indent_size: 2, space_in_empty_paren: true };
+
+  // Initialize copiedStates array to track copy status of each code block
+  const [copiedStates, setCopiedStates] = useState([]);
+
+  // Function to handle copying and update copiedStates
+  const handleCopy = (index) => {
+    const newCopiedStates = [...copiedStates];
+    newCopiedStates[index] = true;
+    setCopiedStates(newCopiedStates);
+    setTimeout(() => {
+      newCopiedStates[index] = false;
+      setCopiedStates(newCopiedStates);
+    }, 1500);
+  };
 
   return (
     <div className="bg-gray-100">
@@ -41,22 +57,34 @@ export default function ArticleDetailCard() {
                   {paragraph.children.map((child, childIndex) => (
                     <div key={childIndex} className="mb-1">
                       {child.code ? (
-                        <div
-                          className="w-full p-4 rounded-lg"
-                          style={{ backgroundColor: "rgb(40, 42, 54)" }}
-                        >
-                          <div className="top mb-2 flex">
-                            <div className="h-3 w-3 bg-red-500 rounded-full"></div>
-                            <div className="ml-2 h-3 w-3 bg-orange-300 rounded-full"></div>
-                            <div className="ml-2 h-3 w-3 bg-green-500 rounded-full"></div>
+                        <div className="relative">
+                          <div className="absolute right-4 top-4 z-10">
+                            <CopyToClipboard
+                              text={child.text}
+                              onCopy={() => handleCopy(index)}
+                            >
+                              <button className="px-2 py-1 bg-blue-500 text-white rounded-md hover:bg-blue-600 focus:outline-none focus:bg-blue-600">
+                                {copiedStates[index] ? "Copied!" : <FaCopy />}
+                              </button>
+                            </CopyToClipboard>
                           </div>
-                          <SyntaxHighlighter
-                            language="javascript"
-                            style={dracula}
-                            showLineNumbers
+                          <div
+                            className="w-full p-4 rounded-lg"
+                            style={{ backgroundColor: "rgb(40, 42, 54)" }}
                           >
-                            {js_beautify(child.text, options)}
-                          </SyntaxHighlighter>
+                            <div className="top mb-2 flex">
+                              <div className="h-3 w-3 bg-red-500 rounded-full"></div>
+                              <div className="ml-2 h-3 w-3 bg-orange-300 rounded-full"></div>
+                              <div className="ml-2 h-3 w-3 bg-green-500 rounded-full"></div>
+                            </div>
+                            <SyntaxHighlighter
+                              language="javascript"
+                              style={dracula}
+                              showLineNumbers
+                            >
+                              {js_beautify(child.text, options)}
+                            </SyntaxHighlighter>
+                          </div>
                         </div>
                       ) : (
                         child.text
